2fd563283d2d530348bdaf672c0e3a261132e3ba
Rick Hurst <rick@potatolondon.com>
2012-11-21 20:32:25 +0000

    finished the paginator and updated to use bootstrap pagination convention

--- siteroot/skin/global/templates/paginator.php
+++ siteroot/skin/global/templates/paginator.php
@@ -1,14 +1,16 @@
-<?php
-
-if($this->current > 1){
-	?>
-	<a href="">&lt;&lt;</a>
+<div class="pagination">
+	<ul>
+		<li class="<?php if($this->current == 1): ?>disabled<?php endif ?>"><a href="<?php echo SITE_ROOT ?><?php if($this->current > 2): ?>posts/all/<?php echo ($this->current -1) ?><?php endif; ?>">&lt;&lt;</a></li>
 	<?php
-}
+
 
 for($i=1; $i<=$this->pages; $i++){
 ?>
-<a class="<?php if($this->current == $i) echo "current" ?>" href="<?php echo SITE_ROOT ?><?php if ($i > 1): ?>posts/all/<?php echo $i ?><?php endif ?>"><?php echo $i ?></a>
+		<li class="<?php if($this->current == $i) echo "active" ?>" ><a href="<?php echo SITE_ROOT ?><?php if ($i > 1): ?>posts/all/<?php echo $i ?><?php endif ?>"><?php echo $i ?></a></li>
 <?php
 }
 ?>
+
+		<li <?php if(!$this->current < $this->pages): ?>class="disabled"<?php endif ?>><a <?php if($this->current < $this->pages): ?>href="<?php echo SITE_ROOT ?>posts/all/<?php echo ($this->current + 1) ?>"<?php endif ?>>&gt;&gt;</a></li>
+	</ul>
+</div>

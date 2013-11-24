d2af9ada4ac840a72167166d154215b3b5d81527
Rick Hurst <rick@potatolondon.com>
2013-11-24 12:13:54 +0000

    about page changed to use block for content

--- siteroot/page_about.php
+++ siteroot/page_about.php
@@ -8,20 +8,9 @@ require ROOT.'/skin/'.SKIN.'/templates/body_top.php';
 
 ?>
 <div class="post">
-	<h2>About Camper Van Things blog</h2>
-
-	<p><img src="/images/misc/campsite_working_wide_700.png" alt="working from a campsite in france" /></p>
-
-	<p>This is a blog written by me, <a href="http://www.rickhurst.co.uk">Rick Hurst</a>, about my experiences working as a web developer on the move. Started as a travel blog in 2010 for a six week summer camping trip with my wife and son touring france in our 1998 vw "new" beetle , it contains insights into working on the road, equipment, tools, camping, experiences and places. Since buying my second VW T25 (T3/Vanagon) camper last year (a 1983 diesel Devon Moonraker), it is now mostly about.. er.. "camper van things", (hence the name!).</p> 
-
-	<p>I work as a contract/ freelance web developer. Working always from a laptop, often in cafes and shared studio space, and sometimes from my car, camper or tent on camping trips. I live in the lovely city of Bristol, UK, with my beautiful wife and son.</p>
-
-	<h3>Sponsorship and advertising</h3>
-	<p>This is a self-funded blog and i'm not currently paid to review any of the equipment, campsites or cafes mentioned. However, I would love to hear of any opportunities, if you have any kit or places that you would like reviewed, or would like to advertise.</p>
-
-	<h3>About eatStatic blog software</h3>
-	<p>This blog runs on a simple piece of open source blogging software that I created for keeping a travel blog - it runs from text files rather than a database, so blog posts can easily be written while offline, then uploaded when an internet connection is available. For more information see <a href="http://eatstatic.olivewoodstudio.com">the eatStatic blog software website</a>.
-
+    <?php
+    echo eatStatic::block("about-page"); 
+    ?>
 </div>
 <?php
 

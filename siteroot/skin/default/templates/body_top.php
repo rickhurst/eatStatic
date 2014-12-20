2023e01c49a3c30dd2a1cf79a62af6ad95ba913d
Rick Hurst <rick.hurst@gmail.com>
2014-12-20 16:43:22 +0000

    set up of default skin, templating stuff

--- siteroot/skin/default/templates/body_top.php
+++ siteroot/skin/default/templates/body_top.php
@@ -1,27 +1,29 @@
 <body>
 
-		<div class="navbar navbar-fixed-top">
-	      <div class="navbar-inner">
-	        <div class="container">
-	          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </a>
-	          <a class="brand" href="/"><?php echo BLOG_TITLE ?></a>
-	          <div class="nav-collapse">
-	            <ul class="nav">
-	              <li class="active"><a href="/">Home</a></li>
-	              <li><a href="/about">About</a></li>
-		      <li><a href="/contact">Contact</a></li> 
-	              <li><a href="/archive">Archive</a></li>
-	              <li><a href="https://github.com/rickhurst/eatStatic">Download</a></li>
-		     </ul>
-	          </div><!--/.nav-collapse -->
-	        </div>
-	      </div>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><?php echo BLOG_TITLE ?></a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+	          <li class="active"><a href="/">Home</a></li>
+	          <li><a href="/about">About</a></li>
+	      	  <li><a href="/contact">Contact</a></li> 
+	          <li><a href="/archive">Archive</a></li>
+	          <li><a href="https://github.com/rickhurst/eatStatic">Download</a></li>
+	     	</ul>
+	      </div><!--/.nav-collapse -->
 	    </div>
+	  </div>
+	</div>
 
-		<div class="container">
-			    <div class="row-fluid">
-    				<div class="span8">
+	<div class="container">
+	    <div class="row-fluid">
+			<div class="span8">
